ce39faa59ccabbf7df1c94680970a4696764df26
deng <978548481@qq.com>
2016-06-20 09:27:29 +0800

    定时加入

--- app.js
+++ app.js
@@ -4,9 +4,14 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
+var schedule = require('node-schedule');
+var rule = new schedule.RecurrenceRule();
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var request = require('request');
+
+var times = [];
+rule.second = times;
 
 var app = express();
 
@@ -18,7 +23,7 @@ app.set('view engine', 'jade');
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -26,10 +31,10 @@ app.use('/', routes);
 app.use('/users', users);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+app.use(function (req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
 // error handlers
@@ -37,24 +42,49 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
+    app.use(function (err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
     });
-  });
 }
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
+app.use(function (err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
 });
-
+rule.hour = times;
+times.push(0);
+var optionsDay = {
+    method: 'GET',
+    encoding: null,
+    url: 'http://120.27.94.166/ranknew/index.php/Home/CrawlerInfo/crawlerDayInfo'
+};
+var optionsMonth = {
+    method: 'GET',
+    encoding: null,
+    url: 'http://120.27.94.166/ranknew/index.php/Home/CrawlerInfo/crawlerDayInfo'
+};
+schedule.scheduleJob(rule, function () {
+    request(optionsDay, function (error, response, body) {
+            if (error) {
+                return console.log(error)
+            }
+        }
+    );
+    request(optionsMonth, function (error, response, body) {
+            if (error) {
+                return console.log(error)
+            }
+        }
+    )
+});
 
 module.exports = app;
---
+++ controler/UpdateTempTables.js
@@ -0,0 +1,9 @@
+/**
+ * Created by deng on 16-6-20.
+ */
+var request = require('request');
+
+
+exports.startUP=function () {
+
+};
