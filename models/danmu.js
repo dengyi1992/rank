780967efc927dc6f25b9406c47990c3a1fca7bbd
deng <978548481@qq.com>
2016-07-07 14:24:12 +0800

    优化插入

--- models/danmu.js
+++ models/danmu.js
@@ -7,6 +7,28 @@ var EventEmitter = require('events').EventEmitter;
 var myEvents = new EventEmitter();
 var conn = mysql.createConnection(config.db);
 var TimeUtils=require('../Utils/TimeUtils');
+Date.prototype.format = function (format) {
+    var o = {
+        "M+": this.getMonth() + 1, //month
+        "d+": this.getDate(), //day
+        "h+": this.getHours(), //hour
+        "m+": this.getMinutes(), //minute
+        "s+": this.getSeconds(), //second
+        "q+": Math.floor((this.getMonth() + 3) / 3), //quarter
+        "S": this.getMilliseconds() //millisecond
+    };
+
+    if (/(y+)/i.test(format)) {
+        format = format.replace(RegExp.$1, (this.getFullYear() + "").substr(4 - RegExp.$1.length));
+    }
+
+    for (var k in o) {
+        if (new RegExp("(" + k + ")").test(format)) {
+            format = format.replace(RegExp.$1, RegExp.$1.length == 1 ? o[k] : ("00" + o[k]).substr(("" + o[k]).length));
+        }
+    }
+    return format;
+};
 exports.DanMuSave=function (platform,roomId,body) {
     myEvents.emit('insertDanMu', platform,roomId,body);
 };
@@ -18,12 +40,11 @@ myEvents.on('insertDanMu', function (platform,roomId,body) {
             console.log(err)
         }
     });
-    var insertSql = 'INSERT INTO ' + tablename + ' (nickname,uid,msg,level,type,device_type) VALUES ?';
+    var insertSql = 'INSERT INTO ' + tablename + ' (nickname,uid,msg,level,type,device_type,ctime) VALUES ?';
     var values=[];
     for (var i = 0; i < body.data.length; i++) {
-
         var item = body.data[i];
-        var insertParams = [item.nn, item.uid, item.txt, item.level, item.type,item.ct];
+        var insertParams = [item.nn, item.uid, item.txt, item.level, item.type,new Date(item.ctime).format("yyyy-MM-dd hh:mm:ss")];
         values.push(insertParams)
     }
     conn.query(insertSql, [values], function (err, rows, field) {
